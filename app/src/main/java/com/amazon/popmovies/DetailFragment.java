b9fcdddee857ee04e2847ed24b9ade752058e381
Jiaming Meng <jiamingm@amazon.com>
2016-12-03 17:41:31 +0800

    Use binding library to simplify the code

--- app/src/main/java/com/amazon/popmovies/DetailFragment.java
+++ app/src/main/java/com/amazon/popmovies/DetailFragment.java
@@ -1,13 +1,13 @@
 package com.amazon.popmovies;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import com.amazon.popmovies.databinding.FragmentDetailBinding;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
@@ -27,29 +27,20 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(DetailActivity.MOVIE_DATA)) {
             Movie movie = intent.getParcelableExtra(DetailActivity.MOVIE_DATA);
             if (movie != null) {
-                ImageView poster = (ImageView) rootView.findViewById(R.id.poster);
-                Picasso.with(getActivity()).load(BASE_IMG_URL + movie.getPosterPath()).into(poster);
-
-                TextView originalTitle = (TextView) rootView.findViewById(R.id.original_title);
-                originalTitle.setText(movie.getOriginalTitle());
-
+                Picasso.with(getActivity()).load(BASE_IMG_URL + movie.getPosterPath()).into(binding.poster);
+                binding.originalTitle.setText(movie.getOriginalTitle());
                 SimpleDateFormat format = new SimpleDateFormat(getString(R.string.release_date_format));
-                TextView releaseDate = (TextView) rootView.findViewById(R.id.release_date);
-                releaseDate.setText(format.format(movie.getReleaseDate()));
-
-                TextView rating = (TextView) rootView.findViewById(R.id.rating);
-                rating.setText(String.valueOf((movie.getVoteAverage())));
-
-                TextView overview = (TextView) rootView.findViewById(R.id.overview);
-                overview.setText(movie.getOverview());
+                binding.releaseDate.setText(format.format(movie.getReleaseDate()));
+                binding.rating.setText(String.valueOf((movie.getVoteAverage())));
+                binding.overview.setText(movie.getOverview());
             }
         }
         // Inflate the layout for this fragment
-        return rootView;
+        return binding.getRoot();
     }
 }
